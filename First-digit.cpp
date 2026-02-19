4e396baa1bd94635892c7d5802270b7d6ee89eea
Hasib Hasan <coderhasibh@gmail.com>
2026-02-19 13:03:55 +0600

    include of basic info

---
+++ First-digit.cpp
@@ -0,0 +1,8 @@
+/*============================================================
+    Codeforces - CF1-P
+    Problem : First digit !
+    Contest :1 Data Types & Conditions
+    Topic   : Data Types & Conditions
+    File    : First-digit.cpp
+    Author  : Hasib Hasan
+============================================================*/
